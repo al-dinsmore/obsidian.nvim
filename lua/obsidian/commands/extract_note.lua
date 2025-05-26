46f682cf877db34ab3e43fb96dac6efd347810ce
al-dinsmore <arrandinsmore@tuta.com>
2025-05-26 16:19:11 +0300

    test without adding text to new file

--- lua/obsidian/commands/extract_note.lua
+++ lua/obsidian/commands/extract_note.lua
@@ -21,12 +21,12 @@ return function(client, data)
 
     -- replace selection with link to new note
     local link = client:format_link(note)
-    vim.api.nvim_buf_set_text(0, row - 1, start_col + 2, row - 1, end_col, { link })
+    vim.api.nvim_buf_set_text(0, row - 1, start_col, row - 1, end_col + 1, { link })
     client:update_ui(0)
 
     -- add the selected text to the end of the new note
     client:open_note(note, { sync = true })
-    vim.api.nvim_buf_set_lines(0, -1, -1, false, string[content])
+    -- vim.api.nvim_buf_set_lines(0, -1, -1, false, string[content])
   end
 
   -----@type string|?
